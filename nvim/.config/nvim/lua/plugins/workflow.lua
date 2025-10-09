37425e4e6e5863da02d4b87bc6a06aaec3178a5f
Michael Long <Michael Longlongmichaeljev@icloud.com>
2025-10-09 16:11:43 +0100

    feat: sessions and overseer

--- nvim/.config/nvim/lua/plugins/workflow.lua
+++ nvim/.config/nvim/lua/plugins/workflow.lua
@@ -4,14 +4,14 @@ return {
 		"windwp/nvim-ts-autotag",
 		opts = {},
 	},
-  {
-    "julienvincent/nvim-paredit",
-    opts = {}
-  },
-  {
-    "ThePrimeagen/refactoring.nvim",
-    opts = {}
-  },
+	{
+		"julienvincent/nvim-paredit",
+		opts = {},
+	},
+	{
+		"ThePrimeagen/refactoring.nvim",
+		opts = {},
+	},
 	{
 		"folke/ts-comments.nvim",
 		opts = {
@@ -43,11 +43,43 @@ return {
 		"johmsalas/text-case.nvim",
 		opts = {},
 	},
-  {
-    "linrongbin16/gentags.nvim",
-    opts = {}
-  },
-  {
-    "liuchengxu/vista.vim", 
-  }
+	{
+		"linrongbin16/gentags.nvim",
+		opts = {},
+	},
+	{
+		"liuchengxu/vista.vim",
+	},
+	{
+		"stevearc/overseer.nvim",
+		opts = {},
+	},
+	{
+		"coffebar/neovim-project",
+		opts = {
+			projects = { -- define project roots
+				"~/projects/*",
+				"~/.config/*",
+			},
+			picker = {
+				type = "telescope", -- one of "telescope", "fzf-lua", or "snacks"
+			},
+		},
+		init = function()
+			-- enable saving the state of plugins in the session
+			vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+		end,
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			-- optional picker
+			{ "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+			-- optional picker
+			{ "ibhagwan/fzf-lua" },
+			-- optional picker
+			{ "folke/snacks.nvim" },
+			{ "Shatur/neovim-session-manager" },
+		},
+		lazy = false,
+		priority = 100,
+	},
 }

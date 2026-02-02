4b6d8224031f34967ef56ce65f6ce5714ba04080
Michael Long <Michael Longlongmichaeljev@icloud.com>
2026-02-02 09:09:02 +0000

    feat: fish

---
+++ fish/.config/fish/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -gx PATH $PATH /opt/nvim-linux-x86_64/bin
+    set -gx PATH $PATH ~/google-cloud-sdk/bin/gcloud
+
+    if not set -q TMUX
+         tmux
+    end
+end
+
+alias --save gpsup='git push --set-upstream origin $(git branch --show-current)'
